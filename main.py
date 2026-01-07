2e7c3548f72153ef55a573c004d655f2d3c2e291
Bugrump <edmcdoughty@gmail.com>
2026-01-07 13:00:22 -0700

    Updated main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import sys
 from stats import sort_on, word_count, character_count, book_report
 
-def get_book_text(path):
-    with open(path) as f:
-        book_text = f.read()
-        return book_text
-
 def main():
     if len(sys.argv) == 1:
         print("Usage: python3 main.py <path_to_book>")
